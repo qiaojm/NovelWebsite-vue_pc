a232f6154bc31393f2e4e1cd21ec5e8d7bcc8260
qiaojm <179888548@qq.com>
2017-03-08 10:06:20 +0800

    modify css

--- server/routes/pay.js
+++ server/routes/pay.js
@@ -3,7 +3,6 @@ var router = express.Router();
 var cfg = require('../config.js');
 
 router.get('/', function (req, res, next) {
-
     res.render('pay', {
         title: "充值",
         dataToRender: {
